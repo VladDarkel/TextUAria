e540471aca169b1b583192744438ef6a61d5d216
Vlad Boikiv <vladboykiv@gmail.com>
2024-11-21 22:09:08 +0200

    exp

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 10)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
