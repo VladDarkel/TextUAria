c8a9902eee73e3e231051c876b1766de9c2f8782
Vlad Boikiv <vladboykiv@gmail.com>
2024-11-28 17:47:38 +0200

    change3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App(props) {
-  const {} = props;
-
+function App() {
   return <p>Hello</p>;
 }
-
-App.propTypes = {};
 
 export default App;
