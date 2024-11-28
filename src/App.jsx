3049760f0b289760bf08bf4d726e090ebf7a9c44
Vlad Boikiv <vladboykiv@gmail.com>
2024-11-28 17:21:55 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import * as Ably from "ably";
-import {
-  AblyProvider,
-  ChannelProvider,
-  useChannel,
-  useConnectionStateListener,
-} from "ably/react";
-import "./index.css";
-
-const client = new Ably.Realtime({
-  key: process.env.VITE_ABLY_API_KEY,
-});
-
 export default function App() {
-  return (
-    <AblyProvider client={client}>
-      <ChannelProvider channelName="get-started">
-        <AblyPubSub />
-        <p>Hello</p>
-      </ChannelProvider>
-    </AblyProvider>
-  );
-}
-
-function AblyPubSub() {
-  const [messages, setMessages] = useState([]);
-
-  useConnectionStateListener("connected", () => {
-    console.log("Connected to Ably!");
-  });
-
-  // Create a channel called 'get-started' and subscribe to all messages with the name 'first' using the useChannel hook
-  const { channel } = useChannel("get-started", "first", (message) => {
-    setMessages((previousMessages) => [...previousMessages, message]);
-  });
-
-  return (
-    // Publish a message with the name 'first' and the contents 'Here is my first message!' when the 'Publish' button is clicked
-    <div>
-      <button
-        onClick={() => {
-          channel.publish("first", "Here is my first message!");
-        }}
-      >
-        Pub
-      </button>
-      {messages.map((message) => {
-        return <p key={message.id}>{message.data}</p>;
-      })}
-    </div>
-  );
+  return <p>Hello</p>;
 }
