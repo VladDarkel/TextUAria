69752991e59508e911158a4b85eb28d53e73596a
VladDarkel <vladboykiv@gmail.com>
2024-11-28 17:42:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import * as Ably from "ably";
-import {
-  AblyProvider,
-  ChannelProvider,
-  useChannel,
-  useConnectionStateListener,
-} from "ably/react";
-import "./index.css";
-import App from "./App.jsx";
-
-const ablyclient = new Ably.Realtime({
-  key: "muB2gA.bsOjzQ:4VtEHW9Y1mutqM7CADDsYqVPzEbaOl8lv9LWoLdSQ-c",
-});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
